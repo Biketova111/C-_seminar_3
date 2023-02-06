aaa41a77f3b13d3c2059be980f87d6d588b70d63
Biketova111 <89241781930@mail.ru>
2023-02-06 15:59:16 +0900

    preparation for the seminar

--- task1/Program.cs
+++ task1/Program.cs
@@ -21,22 +21,3 @@ int GetNumber(string message)
 
     return result;
 }
-
-int GetQuarter(int x, int y)
-{
-    if(x > 0 && y >0)
-        return 1;
-    else if(x < 0 && y >0)
-        return 2;
-    else if(x < 0 && y <0)
-        return 3;
-    else
-        return 4;
-}
-
-int x = GetNumber("Введите х:");
-int y = GetNumber("Введите y:");
-int quarter = GetQuarter(x, y);
-
-Console.WriteLine($"точка с координатами ({x},{y}) лежит в {quarter} четверти");
-
